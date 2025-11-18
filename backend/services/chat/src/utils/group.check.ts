9f85cbee0b716d58cd92a4ff7441072a1d8033e6
xylar-99 <xylar-99@Xylar-99.myguest.virtualbox.org>
2025-11-18 19:15:47 +0100

    fixed some errorsssss

--- backend/services/chat/src/utils/group.check.ts
+++ backend/services/chat/src/utils/group.check.ts
@@ -5,59 +5,28 @@ import { fetchAndEnsureUser } from "./helper";
 
 
 // change later to accept newMemberId and check if it's valid userId 
-export async function checkUserAndFetchGroup( groupId: number , matchId? : string  ) 
+export async function checkUserAndFetchGroup( groupId: number , matchId : string = "" ) 
 {
-  
-  if(matchId !== undefined)
-  {
-     const groupWithMatch = await prisma.group.findUnique({
-        where: { matchId  },
-        include: {
-      members: true,
-      chat: {
-        include: {
-          members: true,
-          messages: {
-            include: {
-              reactions: true,
-            },
-            orderBy: {
-              timestamp: 'desc',
-            },
-          },
-        },
-      },
-    },
-      });
-  
-      if (!groupWithMatch)
-        throw new Error(GroupMessages.FETCH_NOT_FOUND);
-  
-      return groupWithMatch;
-  }
+  const where = (matchId !== "") ? { matchId } : { id: groupId };
 
   const group = await prisma.group.findUnique({
-    where: { id: groupId },
+    where,
     include: {
-      members: true,
+      members: { include: { user: true } },
       chat: {
         include: {
           members: true,
           messages: {
-            include: {
-              reactions: true,
-            },
-            orderBy: {
-              timestamp: 'desc',
-            },
-          },
-        },
-      },
-    },
+            include: { reactions: true },
+            orderBy: { timestamp: 'desc' }
+          }
+        }
+      }
+    }
   });
 
-  if (!group)
-    throw new Error(GroupMessages.FETCH_NOT_FOUND);
+  if (!group) throw new Error(GroupMessages.FETCH_NOT_FOUND);
 
-  return group;
+return group;
+
 }
